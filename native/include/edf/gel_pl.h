d8f13df60e28016a7308c4e705655c91b9354973
Jeremy Erickson <jerickso@cs.unc.edu>
2012-09-20 20:41:44 -0400

    Initial GEL Piecewise Linear implementation

---
+++ native/include/edf/gel_pl.h
@@ -0,0 +1,67 @@
+#ifndef GEL_PL_H
+#define GEL_PL_H
+
+#ifndef SWIG
+#include <vector>
+#endif
+
+class GELPl
+{
+
+ private:
+    std::vector<unsigned long> bounds;
+    int no_cpus;
+    const TaskSet& tasks;
+    int rounds;
+
+    // For faster lookups, to avoid too many conversions.
+    std::vector<double> utilizations;
+
+    double compute_exact_s(double S, const std::vector<double>& Y_ints);
+    double compute_binsearch_s(double S, const std::vector<double>& Y_ints);
+
+    inline double compute_M(double s, double S,
+                                   const std::vector<double>& Y_ints);
+
+    // These are basically just structs that override operator< to allow
+    // sort algorithms to work.
+    class ReplacementType {
+     public:
+        unsigned int old_task;
+        unsigned int new_task;
+        double location;
+
+        bool operator<(const ReplacementType& other) const {
+            return location < other.location;
+        }
+    };
+
+    class TaggedValue {
+     public:
+        unsigned int task;
+        double value;
+
+        //Order is reversed - we are going to want the largest, rather than the
+        //smallest, values.
+        bool operator<(const TaggedValue& other) const {
+            return other.value < value;
+        }
+    };
+
+ public:
+   enum Scheduler {
+     GEDF,
+     GFL
+   };
+
+   GELPl(Scheduler sched,
+         unsigned int num_processors,
+         const TaskSet& tasks,
+         unsigned int rounds);
+
+   unsigned long get_bound(unsigned int index) {
+        return bounds[index];
+   };
+};
+
+#endif
